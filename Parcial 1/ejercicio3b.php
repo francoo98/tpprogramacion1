f309708a5e9b9bced5990048fc649f9b60c15448
Franco Ayala <frma98@gmail.com>
2018-09-13 09:45:27 -0300

    Ejercicio 3

--- Parcial 1/ejercicio3b.php
+++ Parcial 1/ejercicio3b.php
@@ -5,14 +5,19 @@ $usuarios = array(
         "email" => "123@321.com"),
     array("usuario" => "coso",
         "clave" => "qwe",
-        "email" => "123@321.com")
+        "email" => "1233@321.com")
 );
-
+$encontrado = 0;
 for($i = 0; $i < count($usuarios); $i++) {
     if($usuarios[$i]["usuario"] == $_POST["usuario"] and $usuarios[$i]["clave"] == $_POST["password"]) {
         echo "Usuario encontrado<br>";
         echo "Usuario: ".$usuarios[$i]["usuario"]."<br>";
         echo "Email: ".$usuarios[$i]["email"];
+        $encontrado = 1;
         break;
     }
+}
+
+if($encontrado == 0) {
+    echo "No se encontro usuario";
 }
